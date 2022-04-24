04ab7c7a0814d08f494c04e60c6dae6ed80b19b7
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-24 00:51:33 -0500

    Modificando script de pruebas

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,6 +1,20 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require("./../app/spiderman")
+
+describe("Unit test class Spiderman", () => {
+    test('Case 1.- Create Spiderman object', () => {
+      
+      const spiderman1 = new Spiderman("Spider-man", 46, "Tobey Maguire", 3, "SONY");
+      
+      expect(spiderman1.name).toBe("Spider-man");
+      expect(spiderman1.age).toBe(46);
+      expect(spiderman1.actor).toBe("Tobey Maguire");
+      expect(spiderman1.numMovies).toBe(3);
+      expect(spiderman1.studio).toBe("SONY")
     });
+
+    test('Case 2.- Use the method getInfo()', () => {
+      const spiderman1 = new Spiderman("Spider-man", 46, "Tobey Maguire", 3, "SONY");
+
+      expect(spiderman1.getInfo()).toBe("Hey, I'm Tobey Maguire from SONY studio")
+    })
   })
