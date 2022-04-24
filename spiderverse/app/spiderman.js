5a4a02faf012986dd68ca39f1d76e042486c5819
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-24 00:32:28 -0500

    Prueba 1 terminada y aprobada

---
+++ spiderverse/app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+    constructor(name, age, actor, numMovies, studio){
+        this.name = name;
+        this.age = age;
+        this.actor = actor;
+        this.numMovies = numMovies;
+        this.studio = studio;
+    }
+}
+
+module.exports = Spiderman;
